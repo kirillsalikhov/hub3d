07c9de63848f793b4a1b3c157d93ef7d55870ee6
kirill <kirill.salikhov@gmail.com>
2023-02-23 18:38:45 +0400

    initial rails setup (minimal + pg), initial integration setup (rails + pg)

---
+++ integration/bin/main.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+usage="
+Script usage:
+  scripts/main.sh [--dev=DEV_LETTERS] {up|down|stop|other docker-compose cmd}
+
+DEV_LETTERS:
+  b - backend : store_backend
+Examples:
+  main.sh up // all container in prod mode
+
+  // start certain containers in dev mode
+  main.sh --dev=b up // backend
+  ----
+"
+
+cd "$(dirname "$0")"
+
+if [[ -z $@ ]]; then
+    echo "$usage"
+fi
+
+ARGS=()
+dev=''
+for i in "$@"
+do
+    if [[ $i == "--dev="* ]]
+    then
+        dev="${i#*=}"
+        shift
+    else
+        ARGS+=("$i")
+    fi
+done
+
+prod_compose="-f ../compose/hub.yml"
+
+dev_compose=()
+
+for l in $(echo ${dev} | grep -o .)
+do
+    case $l in
+        b)
+            echo "DEV backend : backend"
+            dev_compose+=" -f ../compose/dev/backend.yml"
+        ;;
+    esac
+done
+
+docker-compose ${prod_compose} ${dev_compose} -p hub "${ARGS[@]}"
---
+++ integration/bin/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+compose_files="-f ../compose/hub.yml"
+
+docker-compose ${compose_files} -p hub pull
+
+#docker-compose ${compose_files} -p hub up -d postgres
+#
+#sleep 3
+#
+## TODO consider move it to be container
+docker-compose ${compose_files} -p hub run -e RAILS_ENV=development backend rake db:setup
+
+docker-compose ${compose_files} -p hub down
