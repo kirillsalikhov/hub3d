dafa8268a35b6da5723ca47a2dfe19348fef8531
kirill <kirill.salikhov@gmail.com>
2023-07-25 21:37:10 +0400

    dummy routes for resource convert_create

--- backend/app/models/store/resource.rb
+++ backend/app/models/store/resource.rb
@@ -1,2 +1,4 @@
 class Store::Resource < ApplicationRecord
+  # TODO dependent: :destroy to versions
+  has_many :versions, class_name: 'Store::Version'
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :resources do
       get 'convert_new', on: :collection
       post 'convert_create', on: :collection
+
+      # TODO remove
+      get 'foobar', on: :collection
+      get 'testjob', on: :collection
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
