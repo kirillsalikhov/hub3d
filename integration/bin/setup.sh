bedd6b069f9004f7f14265ad8a3ebb8f3b3bdc99
kirill <kirill.salikhov@gmail.com>
2023-09-13 13:08:21 +0000

    build: add default-bucket creation to setup.sh
    refs: HUB-3-D-T-16
    
    
    Co-authored-by: Kirill Salikhov <Kirill.Salikhov@gmail.com>
    
    
    Merge-request: HUB-3-D-MR-5
    Merged-by: Marina Starostina <starostinamb@gmail.com>

--- integration/bin/setup.sh
+++ integration/bin/setup.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+function load_env() {
+  source ./../compose/.env
+}
+
+function minio_setup() {
+  # -net=host - needed for 127.0.0.1 to be host
+  # default-bucket - our name
+  docker run --net=host --entrypoint sh minio/mc -c "
+    mc config host add minio http://127.0.0.1:10000 $HUB__MINIO_ROOT_USER $HUB__MINIO_ROOT_PASSWORD
+    mc mb minio/default-bucket
+    "
+}
 cd "$(dirname "$0")"
 
 compose_files="-f ../compose/hub.yml"
 
 docker compose ${compose_files} -p hub pull
+
+# minio setup start
+docker compose ${compose_files} -p hub up -d minio_for_backend
+sleep 1
+load_env
+minio_setup
+# minio end start
+
 
 docker compose ${compose_files} -p hub run -e RAILS_ENV=development backend rake db:setup
 
