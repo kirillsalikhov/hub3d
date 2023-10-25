8fba14470ed65d8268a14f37fff97b7585f59f54
marina <starostinamb@gmail.com>
2023-10-25 14:30:40 +0300

    replace bg with gradient

--- backend/app/frontend/components/Layout/index.jsx
+++ backend/app/frontend/components/Layout/index.jsx
@@ -2,7 +2,7 @@ import { Header } from '../Header';
 
 export default function Layout({ children }) {
     return (
-        <div className="min-h-full relative overflow-hidden bg-[url('../images/bg.png')] bg-cover">
+        <div className="min-h-full relative overflow-hidden bg-front bg-cover">
             <div className="relative pb-16 pt-6 sm:pb-24">
                 <Header/>
                 <main className="mt-16 sm:mt-24">
