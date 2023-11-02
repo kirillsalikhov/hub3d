e5a05250f01a78773633a155976d50c6d1c3bb83
marina <starostinamb@gmail.com>
2023-11-02 11:04:30 +0000

    add parking preset for IA; fix fonts
    Co-authored-by: Marina Starostina <starostinamb@gmail.com>
    
    
    Merge-request: HUB-3-D-MR-19
    Merged-by: Kirill Salikhov <Kirill.Salikhov@gmail.com>

--- backend/app/frontend/components/Industrial/index.js
+++ backend/app/frontend/components/Industrial/index.js
@@ -1,14 +1,19 @@
-import IndustrialApp from '@wg/industrial-app';
+import IndustrialApp, { presets } from '@wg/industrial-app';
 import { dataLoader } from './data-loader';
 
+const parkingIndex = presets.findIndex(preset => preset.name === 'Parking');
+const [ parkingPreset ] = presets.splice(parkingIndex, 1);
+const rearrangedPresets = [ parkingPreset, ...presets ];
+
 window.viewer = {
-    load: async ({resource, versionContents, domElement}) => {
+    load: async ({ resource, versionContents, domElement }) => {
         const config = {
+            presets: rearrangedPresets,
             integration: {
                 enabled: true,
                 share: [
                     { title: 'Copy link', value: location.href },
-                    { title: 'Copy public embed code', value: `<iframe src="${location.origin + location.pathname}" width="800" height="600" />` }
+                    { title: 'Copy public embed code', value: `<iframe src="${ location.origin + location.pathname }" width="800" height="600" />` }
                 ],
                 // versionName: 'v' + version.version,
                 // versionId: version.id,
@@ -31,7 +36,7 @@ window.viewer = {
             }
             industrialApp.set(manifest);
         } else {
-            industrialApp.set({ model: { url: 'model.wmd' }});
+            industrialApp.set({ model: { url: 'model.wmd' } });
         }
     }
 };
