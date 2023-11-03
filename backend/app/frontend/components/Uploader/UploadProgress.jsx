70d121fba40512e261109c86da10e57b43a0f0bd
marina <starostinamb@gmail.com>
2023-11-03 21:52:35 +0300

    fix transition

--- backend/app/frontend/components/Uploader/UploadProgress.jsx
+++ backend/app/frontend/components/Uploader/UploadProgress.jsx
@@ -8,7 +8,7 @@ export const UploadProgress = ({ file, progress }) => {
         <div className="rounded-lg h-full">
             {
                 <div id={ `upload-${ uploadId }` } className="relative h-full rounded-md bg-gray-100 shadow-inner">
-                    <div className={ `motion-safe:animate-pulse transition-width duration-${progressTransitionDuration} ease-linear h-full rounded-md bg-gradient-to-tr from-indigo-600/60 to-purple-700/60` } style={{ width: `${progress * 100}%` }} />
+                    <div className={ `motion-safe:animate-pulse transition-all duration-${progressTransitionDuration} ease-linear h-full rounded-md bg-gradient-to-tr from-indigo-600/60 to-purple-700/60` } style={{ width: `${progress * 100}%` }} />
                     <div className="absolute top-1/4 p-6 text-center overflow-hidden">
                         <p className="text-xl font-medium text-blue-950">Uploading {filename}...</p>
                         <p className="mt-4 text-sm leading-5 text-blue-950">
