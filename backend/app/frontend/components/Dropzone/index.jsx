221098c27f48bc3e07506d30961c40615a7ac9df
marina <starostinamb@gmail.com>
2023-11-06 01:39:03 +0300

    fix dropzone border; remove monkeycode

--- backend/app/frontend/components/Dropzone/index.jsx
+++ backend/app/frontend/components/Dropzone/index.jsx
@@ -26,8 +26,8 @@ const DropzoneContent = ({rootProps, inputProps, isDragActive}) => (
             <div htmlFor="dropzone-file-upload" { ...rootProps }>
                 <input id="dropzone-file-upload" name="dropzone-file-upload" type="file"
                        className="sr-only" { ...inputProps } />
-                <div className={ `${ isDragActive ? 'bg-gradient-to-tr' : '' } group cursor-pointer relative flex rounded-lg h-80 bg-white bg-dropzone from-blue-500 via-pink-300 via-70% to-purple-600 border-2 border-dashed border-gray-300 hover:border-transparent` }>
-                    <div className="flex w-full bg-white rounded-md text-center items-center justify-center">
+                <div className={ `${ isDragActive ? 'bg-gradient-to-tr' : '' } group cursor-pointer relative flex rounded-lg h-80 bg-white hover:bg-gradient-to-tr from-blue-500 via-pink-300 via-70% to-purple-600 border-2 border-dashed border-gray-300 hover:border-none` }>
+                    <div className="flex w-full m-0.5 bg-white rounded-md text-center items-center justify-center">
                         <div>
                             <div className="m-auto w-14 h-14 rounded-full p-4 text-white bg-gradient-to-tr from-indigo-600/80 to-purple-700/80 shadow-md shadow-indigo-500/40">
                                 <PlusIcon className="w-6 h-6" />
