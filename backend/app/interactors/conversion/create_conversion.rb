f2c278ddf23bd11425673e11269b8cc8deda5498
kirill <kirill.salikhov@gmail.com>
2023-12-13 21:49:07 +0400

    wip(backend): add choose_server to Conversion::CreateConversion interactor
    refs: HUB-3-D-T-56

--- backend/app/interactors/conversion/create_conversion.rb
+++ backend/app/interactors/conversion/create_conversion.rb
@@ -2,6 +2,7 @@ class Conversion::CreateConversion
   include Interactor
 
   def call
+    choose_server
     create_conversion_job
     create_conversion_task
 
@@ -13,17 +14,26 @@ class Conversion::CreateConversion
 
   private
 
+  def choose_server
+    filename, byte_size = context.input.values_at(:filename, :byte_size)
+    @cs_server = Conversion::AvailableHostQuery.new
+      .call(context.recipe, filename.to_s, byte_size)
+    @cs_server_url = Conversion.get_server(@cs_server)[:base_url]
+  end
+
   def create_conversion_job
-    @conversion_job_id = Conversion::Client.new().create_job(
-      input: context.input.url(expires_in: 1.week),
-      recipe: context.recipe
-    )
+    @conversion_job_id = Conversion::Client.new(@cs_server_url)
+      .create_job(
+        input: context.input.url(expires_in: 1.week),
+        recipe: context.recipe
+      )
   end
 
   def create_conversion_task
     # TODO move default status and progress to inializer or whatever
     @conversion_task = Store::ConversionTask.new(
       conversion_job_id: @conversion_job_id,
+      cs_server: @cs_server,
       on_success: context.on_success,
       status: Store::ConversionTask::STATUSES[:in_progress],
       progress: 0,
