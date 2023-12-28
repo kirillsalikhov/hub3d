e84a856a0cb3f19641a28017994445a27846be1f
kirill <kirill.salikhov@gmail.com>
2023-12-28 17:40:20 +0400

    chore: commit schema.rb

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_20_111033) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_13_163243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_20_111033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "meta"
+    t.string "cs_server"
   end
 
   create_table "store_resources", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
