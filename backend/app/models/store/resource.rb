f6f1256fdd3771743a0d3d3c6bf838d4a05fc4b8
kirill <kirill.salikhov@gmail.com>
2024-01-08 16:35:00 +0400

    chore: set default ShareOptions to Resource when new resource
    refs: HUB-3-D-T-51

--- backend/app/models/store/resource.rb
+++ backend/app/models/store/resource.rb
@@ -1,6 +1,14 @@
 class Store::Resource < ApplicationRecord
+  after_initialize :default_values,  if: :new_record?
+
   # TODO dependent: :destroy to versions
   has_many :versions, class_name: "Store::Version"
   belongs_to :author, class_name: "User", optional: true
   has_one :share_options, class_name: "Store::ShareOptions", dependent: :destroy
+
+  private
+
+  def default_values
+    self.share_options ||= Store::ShareOptions.new
+  end
 end
