fbd85d304028d0da26401986d6bc60a2cbd7c02c
kirill <kirill.salikhov@gmail.com>
2024-01-15 21:11:51 +0400

    feat: catch ActiveRecord::RecordInvalid in Api::Aplication controller
    refs: HUB-3-D-T-51

--- backend/app/controllers/api/application_controller.rb
+++ backend/app/controllers/api/application_controller.rb
@@ -1,3 +1,9 @@
 class Api::ApplicationController < ActionController::API
   include GuestConcern
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+  def render_unprocessable_entity(e)
+    render json: {errors: e.record.errors}, status: :unprocessable_entity
+  end
 end
